836ce1f95f13c30c3732d1bb8ea2d0b1f24613a5
konstantin0s <constantintofan85@gmail>
2018-03-21 20:44:10 +0100

    Add shopping list functionality

--- js/app.js
+++ js/app.js
@@ -11,3 +11,61 @@ window.fn.load = function(page) {
   content.load(page)
     .then(menu.close.bind(menu));
 };
+
+
+document.addEventListener('init', function(event) {
+  if(event.target.id == 'home') {
+    openDb();
+    getItems();
+  }
+})
+
+var db = null;
+
+function onError(tx, e) {
+  alert('Something went wrong' + e.Message);
+}
+
+function onSuccess(tx, e) {
+getItems();
+}
+
+function openDb() {
+  db = openDatabase("ShoppingList", "1", "Shopping List", 1024*1024);
+
+  db.transaction(function(tx) {
+    tx.executeSql("CREATE TABLE IF NOT EXISTS items (ID INTEGER PRIMARY KEY ASC, item TEXT)", []);
+    });
+}
+
+function getItems() {
+  db.transaction(function(tx) {
+    tx.executeSql("SELECT * FROM items", [], renderItems, onError);
+  });
+}
+
+function renderItems(tx, rs) {
+  var output = "";
+  var list = document.getElementById("shoppinglist");
+
+  for(i = 0; i < rs.rows.length; i++) {
+  var row = rs.rows.item(i);
+  output += "<ons-list-item>" + row.item +
+  "<div class=\"right\"> <ons-button><ons-icon icon=\"trash\"></ons-icon></ons-button></div>" +
+  "</ons-list-item>";
+   }
+
+   list.innerHTML = output;
+}
+
+function addItem() {
+  var textbox = document.getElementById("item");
+  var value = textbox.value;
+
+  db.transaction(function(tx) {
+    tx.executeSql("INSERT INTO items (item) VALUES (?)", [value], onSuccess, onError);
+  });
+
+  textbox = "";
+  fn.load("home.html")
+}
